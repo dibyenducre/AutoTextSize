c2c1b3f27fd035350ac3e47ccd2dbdd8c8819d76
dibyenducre <dibyendu.cre@gmail.com>
2019-01-10 13:26:07 +0530

    - Auto Textsize

---
+++ app/src/main/java/com/app/autotextsize/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.app.autotextsize
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            textView.setText(editText.text.toString())
+        }
+
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+
+                textView.setText(editText.text.toString())
+
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        })
+
+    }
+}
